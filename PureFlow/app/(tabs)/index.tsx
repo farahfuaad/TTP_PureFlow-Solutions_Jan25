8fb3fde0f14331b71406d200113b5fde6a910120
Farah <farah.fuaad@gmail.com>
2025-03-20 15:20:27 +0800

    app updates

--- PureFlow/app/(tabs)/index.tsx
+++ PureFlow/app/(tabs)/index.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import {Image, StyleSheet, View, Text, Pressable} from "react-native";
-import Vector1 from "../assets/Vector-1.svg"
-import Vector2 from "../assets/Vector-2.svg"
+import Vector1 from "../../assets/Vector-1.svg";
+import Vector2 from "../../assets/Vector-2.svg";
 
 const Page = () => {
   	
   	return (
     		<View style={styles.page}>
-      			<View style={styles.blueModernWaterCompanyLogoParent}>
-        				<Image style={styles.blueModernWaterCompanyLogo} resizeMode="cover" source={require("../assets/BlueModernWaterCompanyLogoType1.png")} />
+      			<View style={styles.vectorParent}>
         				<Vector1 style={[styles.groupChild, styles.groupPosition]} width={599} height={251} />
         				<Vector2 style={[styles.groupItem, styles.groupPosition]} width={526} height={267} />
         				<Pressable style={[styles.btnnext, styles.btnnextLayout]} onPress={()=>{}}>
@@ -16,6 +15,11 @@ const Page = () => {
           					<Text style={styles.next}>NEXT</Text>
         				</Pressable>
       			</View>
+      			<Image
+        				style={styles.bluemodernwatercompanylogotypeIcon}
+        				resizeMode="cover"
+        				source={require('@/assets/images/BlueModernWaterCompanyLogoType1.png')}
+      			/>
     		</View>);
 };
 
@@ -29,13 +33,6 @@ const styles = StyleSheet.create({
     		width: 318,
     		position: "absolute"
   	},
-  	blueModernWaterCompanyLogo: {
-    		height: 105,
-    		width: 318,
-    		left: 160,
-    		top: 0,
-    		position: "absolute"
-  	},
   	groupChild: {
     		bottom: 16,
     		left: 0
@@ -45,11 +42,10 @@ const styles = StyleSheet.create({
     		left: 115
   	},
   	background: {
+    		top: 0,
     		borderRadius: 8,
     		backgroundColor: "#fff",
-    		left: 0,
-    		height: 60,
-    		top: 0
+    		left: 0
   	},
   	next: {
     		top: 19,
@@ -63,15 +59,21 @@ const styles = StyleSheet.create({
     		position: "absolute"
   	},
   	btnnext: {
-    		top: 407,
-    		left: 160,
-    		height: 60
+    		top: 66,
+    		left: 160
   	},
-  	blueModernWaterCompanyLogoParent: {
-    		top: 315,
+  	vectorParent: {
+    		bottom: -111,
     		left: -131,
     		width: 640,
-    		height: 608,
+    		height: 267,
+    		position: "absolute"
+  	},
+  	bluemodernwatercompanylogotypeIcon: {
+    		top: 301,
+    		left: 29,
+    		height: 105,
+    		width: 318,
     		position: "absolute"
   	},
   	page: {
